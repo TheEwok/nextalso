fd18d1634c6b92f44f5759a421e85e8a3f92184a
Baz O'Mahony <bomahony@shutterstock.com>
2015-06-25 10:28:48 -0600

    Pushing to github

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-nextalso
-========
+# nextalso
+
+### Flow control using event emitters.
+NextAlso is an asynchronus flow control library for Node.js that uses event
+emitters to signal when a particular group of functions have all completed.
+
+Currently NextAlso handles series and parallel execution of asynchronous
+functions. It also offers introspection into the current execution state by
+emitting events as each step is completed.
+
+NextAlso uses method chaining to build expressive and explicit code that is
+easy to understand.
+
+### Examples
+##### Execute functions in series
+
+```javascript
+var fs = require("fs");
+var NextAlso = require("nextalso");
+var flow_control = new NextAlso();
+
+flow_control.next(fs.readFile,['./test_file.txt'])
+    .next(fs.readFile,['./test_file_2.txt'])
+    .next(fs.readFile,['./test_file_3.txt'])
+    .on("series_step",function(curr_results) {
+        console.log("Series Step Fired -");
+        console.log(curr_results);
+    })
+    .on("series_success",function(results) {
+        console.log("Series completed successfully");
+        console.log(resulte);
+    });
+```
+
+# ToDo
+
+* Add logging lib
+* Run both types grouped as chained
+* Add debug events
+* Waterfall results array through series
+* Documentation
+* Bind apply
+* Warn if last param provided is fn
--- test/lib.nextalso.js
+++ test/lib.nextalso.js
@@ -22,128 +22,132 @@ describe("NextAlso Flow Control",function(){
         fake_step_function = function(step,cb) { cb(null,step); };
     });
     context("Parallel calls",function() {
-        it("should return itself when also() is called",function() {
+        it("should return itself when also() is called",function(done) {
             expect(nextalso.also(fake_function,["FAKE PARAMETER"])).to.be.an('object');
+			done();
         });
-
-        it("should register a new parallel step when also is called",function() {
-            nextalso.also(fake_function,["FAKE PARAMETER"]);
-            expect(nextalso.getParallelSteps()).to.have.length(1);
-            expect(nextalso.getParallelSteps()[0].params[0]).to.equal("FAKE PARAMETER");
-        });
-        it("should register two steps when also is called twice",function() {
-            nextalso.also(fake_function,["FAKE PARAMETER"]);
-            nextalso.also(fake_function,["FAKE PARAMETER"]);
-            expect(nextalso.getParallelSteps()).to.have.length(2);
-        });
-        it("should call the parallel function",function() {
-            nextalso.also(fake_function,["FAKE PARAMETER"]);
-            nextalso.parallel();
+        it("should call the parallel function",function(done) {
+            nextalso.also(fake_function,"FAKE PARAMETER");
+            nextalso.start();
             expect(fake_function).to.have.been.called;
             expect(fake_function).to.have.been.calledWith("FAKE PARAMETER");
+			done();
         });
-        it("should call the parallel function with all arguments",function() {
-            nextalso.also(fake_function,["FAKE PARAMETER 1","FAKE PARAMETER 2"]);
-            nextalso.parallel();
+        it("should call the parallel function with all arguments",function(done) {
+            nextalso.also(fake_function,"FAKE PARAMETER 1","FAKE PARAMETER 2");
+            nextalso.start();
             expect(fake_function).to.have.been.called;
             expect(fake_function).to.have.been.calledWith("FAKE PARAMETER 1","FAKE PARAMETER 2");
+			done();
         });
-        it("should emit parallel_success when done",function() {
+        it("should emit parallel_success when done",function(done) {
             var emit_spy = sinon.spy();
             nextalso.on("parallel_success",emit_spy);
             nextalso.also(fake_callback_function);
-            nextalso.parallel();
+            nextalso.start();
             expect(emit_spy).to.have.been.called;
+			done();
         });
-        it("should return an array when done",function() {
+        it("should return an array when done",function(done) {
             var emit_spy = sinon.spy();
             nextalso.on("parallel_success",emit_spy);
             nextalso.also(fake_callback_function);
-            nextalso.parallel();
+            nextalso.start();
             expect(emit_spy).to.have.been.called;
             expect(emit_spy).to.have.been.calledWithExactly(["FAKE_FN_PARAM"]);
+			done();
         });
-        it("should have two results for two calls",function() {
+        it("should have two results for two calls",function(done) {
             var emit_spy = sinon.spy();
             nextalso.on("parallel_success",emit_spy);
             nextalso.also(fake_callback_function);
             nextalso.also(fake_callback_function);
-            nextalso.parallel();
+            nextalso.start();
             expect(emit_spy).to.have.been.called;
             expect(emit_spy).to.have.been.calledWithExactly(["FAKE_FN_PARAM","FAKE_FN_PARAM"]);
+			done();
         });
-        it("should emit parallel_step and step once",function() {
+        it("should emit parallel_step once and step twice",function(done) {
             var emit_spy = sinon.spy();
             var emit_spy2 = sinon.spy();
             nextalso.on("parallel_step",emit_spy);
             nextalso.on("step",emit_spy2);
             nextalso.also(fake_callback_function);
             nextalso.also(fake_callback_function);
-            nextalso.parallel();
-            expect(emit_spy).to.have.been.called;
-            expect(emit_spy2).to.have.been.called;
+            nextalso.start();
+            expect(emit_spy).to.have.been.calledOnce;
+            expect(emit_spy2).to.have.been.calledTwice;
+			done();
         });
-        it("should emit parallel_error",function() {
+        it("should emit parallel_error",function(done) {
             var emit_spy = sinon.spy();
             nextalso.on("parallel_error",emit_spy);
             nextalso.also(fake_callback_function_err);
-            nextalso.parallel();
+            nextalso.start();
             expect(emit_spy).to.have.been.called;
+			done();
         });
-        it("should emit with an error",function() {
+        it("should emit with an error",function(done) {
             var emit_spy = sinon.spy();
             nextalso.on("parallel_error",emit_spy);
             nextalso.also(fake_callback_function_err);
-            nextalso.parallel();
-            expect(emit_spy).to.have.been.calledWithExactly("ERROR");
+            nextalso.start();
+            expect(emit_spy).to.have.been.calledWithExactly("ERROR",[]);
+			done();
         });
-        it("should not emit success if there was an error",function() {
+        it("should not emit success if there was an error",function(done) {
             var emit_spy = sinon.spy();
             var emit_spy2 = sinon.spy();
             nextalso.on("parallel_error",emit_spy);
             nextalso.also(fake_callback_function_err);
             nextalso.also(fake_callback_function);
-            nextalso.parallel();
-            expect(emit_spy).to.have.been.calledWithExactly("ERROR");
+            nextalso.start();
+            expect(emit_spy).to.have.been.calledWithExactly("ERROR",["FAKE_FN_PARAM"]);
             expect(emit_spy2).to.not.have.been.called;
+			done();
         });
 
     });
     context("Series calls",function() {
-        it("should return itself when next is called",function() {
+        it("should return itself when next is called",function(done) {
             expect(nextalso.next(fake_function)).to.be.an("object");
+			done();
         }); 
-        it("should emit success when complete",function() {
+        it("should emit success when complete",function(done) {
             var emit_spy = sinon.spy();
             nextalso.on("series_success",emit_spy);
             nextalso.next(fake_callback_function);
-            nextalso.series();
+            nextalso.start();
             expect(emit_spy).to.have.been.called;
             expect(emit_spy).to.have.been.calledWith(["FAKE_FN_PARAM"]);
+			done();
         });
-        it("should emit series_step between series functions",function() {
+        it("should emit series_step between series functions",function(done) {
             var emit_spy = sinon.spy();
             nextalso.on("series_step",emit_spy);
             nextalso.next(fake_step_function,[1]);
             nextalso.next(fake_step_function,[2]);
-            nextalso.series();
-            expect(emit_spy).to.have.been.calledTwice;
+            nextalso.start();
+            expect(emit_spy).to.have.been.called;
+			done();
         });
-        it("should emit step between series functions",function() {
+        it("should emit step between series functions",function(done) {
             var emit_spy = sinon.spy();
             nextalso.on("step",emit_spy);
             nextalso.next(fake_step_function,[1]);
             nextalso.next(fake_step_function,[2]);
-            nextalso.series();
+            nextalso.start();
             expect(emit_spy).to.have.been.calledTwice;
+			done();
         });
-        it("should emit series_step with results in correct order",function() {
+        it("should emit series_step with results in correct order",function(done) {
             var emit_spy = sinon.spy();
             var spy_emit_mess_one,spy_emit_mess_two;
             nextalso.on("series_step",emit_spy);
-            nextalso.next(fake_step_function,["SHOULD BE FIRST"]);
-            nextalso.next(fake_step_function,["SHOULD BE SECOND"]);
-            nextalso.series();
+            nextalso.on("series_success",emit_spy);
+            nextalso.next(fake_step_function,"SHOULD BE FIRST");
+            nextalso.next(fake_step_function,"SHOULD BE SECOND");
+            nextalso.start();
             spy_emit_mess_one = emit_spy.getCall(0);
             spy_emit_mess_two = emit_spy.getCall(1);
             expect(emit_spy).to.have.been.calledTwice;
@@ -151,7 +155,27 @@ describe("NextAlso Flow Control",function(){
             expect(Array.isArray(spy_emit_mess_two.args[0])).to.be.true;
             expect(spy_emit_mess_one.args[0][0]).to.equal("SHOULD BE FIRST");
             expect(spy_emit_mess_two.args[0][1]).to.equal("SHOULD BE SECOND");
+			done();
         });
-
+		it("should emit series_error",function(done) {
+			var emit_spy = sinon.spy();
+			nextalso.on("series_error",emit_spy);
+			nextalso.next(fake_callback_function_err);
+			nextalso.start();
+			expect(emit_spy).to.have.been.calledOnce;
+            expect(emit_spy).to.have.been.calledWithExactly("ERROR",[]);
+			done();
+		});
+		it("should emit series_error and not call success",function(done) {
+			var emit_spy = sinon.spy();
+			var emit_spy_success = sinon.spy();
+			nextalso.on("series_error",emit_spy);
+			nextalso.on("series_success",emit_spy_success);
+			nextalso.next(fake_callback_function_err);
+			nextalso.start();
+			expect(emit_spy).to.have.been.calledOnce;
+			expect(emit_spy_success).to.not.have.been.called;
+			done();
+		});
     });
 });
